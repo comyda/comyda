7c9bdaaeefc30b1f9f67af26fa598d65300f8d6d
Rayana <rayana.goncalves.cunha@hotmail.com>
2016-09-13 16:09:21 -0300

    setando caminho para os diretórios public e views

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.render('index');
+});
+app.get('/evento', function (req, res) {
+		res.render('planilha');
 });
 
 app.listen(3000, function () {
