7a24eba53ed480cbe6ecc2845b714e8a2c4a3cb8
Leonardo <leooliveira3471@hotmail.com>
2016-11-23 16:29:19 -0300

    Para ray baixar

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', function (req, res) {
-<<<<<<< 98657fcc5385df8cbd11cb7c36b3dfda043681d3
 var url = 'mongodb://localhost:27017/oxifood';
 
  // Use connect method to connect to the server
@@ -93,115 +92,17 @@ var url = 'mongodb://localhost:27017/oxifood';
  		// Use connect method to connect to the server
  		MongoClient.connect(url, function(err, db) {
 
- var collection = db.collection('participar');
+ 			var collection = db.collection('participar');
 
  				var dados = {
- 		    _id: uuid.v4(),
- 				eventid: req.body.eventid,
- 				firstname: req.body.firstname,
- 				restriction: req.body.restriction
+	 		    _id: uuid.v4(),
+	 				eventid: req.body.eventid,
+	 				firstname: req.body.firstname,
+	 				restriction: req.body.restriction
  				};
 
- 							collection.insertOne(dados);
-=======
-	var url = 'mongodb://localhost:27017/oxifood';
-
-// Use connect method to connect to the server
-	MongoClient.connect(url, function(err, db) {
-
-		var collection = db.collection('eventos');
-		collection.find({}).toArray(function(err, docs) {
-
-			for (var i = 0; i < docs.length; i++) {
-				var day = docs[i].time.getDate();
-				if (day < 10) {
-					day = '0' + day;
-				}
-				var month = docs[i].time.getMonth()+1;
-				if (month < 10) {
-					month = '0' + month;
-				}
-				var hours = docs[i].time.getHours() + 3;
-				if (hours < 10) {
-					hours = '0' + hours;
-				}
-				var minutes = docs[i].time.getMinutes();
-				if (minutes < 10) {
-					minutes = '0' + minutes;
-				}
-				docs[i].timeAsString = day + '/' + month + ' ' + hours+ ':'+ minutes;
-				}
-
-							res.render('novaindex', {eventos:docs});
-				   });
-
-		db.close();
-	});
-});
-
-app.get('/eventos/:id', function (req, res) {
-	var url = 'mongodb://localhost:27017/oxifood';
-
-// Use connect method to connect to the server
-	MongoClient.connect(url, function(err, db) {
-
-		var collection = db.collection('participar');
-		collection.find({eventid:req.params.id}).toArray(function(err, docs) {
-
-							res.render('planilha', {participar:docs, eventid:req.params.id});
-				   });
-
-					 db.close();
-	});
-});
-
-
-app.get('/evento', function(req, res) {
-	res.render('criar');
-});
-
-app.post('/eventos', function (req, res) {
-	var url = 'mongodb://localhost:27017/oxifood';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-
-	var collection = db.collection('eventos');
-
-		var event = {
-			_id: uuid.v4(),
-			name: req.body.name,
-			ownername: req.body.ownername,
-			time: new Date(req.body.time)
-		};
-
-					collection.insertOne(event);
-					res.redirect('/');
-
-				db.close();
-
-	});
-});
-
-app.post('/participar', function (req, res) {
-	var url = 'mongodb://localhost:27017/oxifood';
-
-
-		// Use connect method to connect to the server
-		MongoClient.connect(url, function(err, db) {
-
-var collection = db.collection('participar');
-
-				var dados = {
-		    _id: uuid.v4(),
-				eventid: req.body.eventid,
-				firstname: req.body.firstname,
-				restriction: req.body.restriction
-				};
-
-							collection.insertOne(dados);
->>>>>>> Para ray baixar
-						res.redirect('/');
+ 				collection.insertOne(dados);
+				res.redirect('/');
 
  			  db.close();
  		});
