da0ddf76008d35959b85f35be6842bc6481160ae
myllena <myllenaalvesdelima@outlook.com>
2016-12-21 15:42:04 -0300

    Transformando em números inteiros

--- services/calcular.js
+++ services/calcular.js
@@ -1,11 +1,11 @@
 module.exports = {
   getPizzas: function(subscribers) {
-    var numPizza = subscribers/4;
+    var numPizza = Math.ceil(subscribers/4);
     var totalPizza = numPizza * 27.00;
     return {numPizza, total: totalPizza};
-  },
+},
   getGuarana: function(subscribers) {
-    var numGuarana = subscribers/8;
+    var numGuarana = Math.ceil(subscribers/8);
     var totalGuarana = numGuarana * 8.00;
     return {numGuarana, total: totalGuarana}
   }
