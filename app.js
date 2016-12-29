655be101efdb363eafd5bf67b5b7882b7e6188bf
Leonardo <leooliveira3471@hotmail.com>
2016-12-29 14:30:22 -0300

    Adicionando restaurante

--- app.js
+++ app.js
@@ -150,6 +150,33 @@ var url = 'mongodb://localhost:27017/oxifood';
  		});
  });
 
+ app.get('/restaurante', function(req, res) {
+ 	res.render('addrestaurante');
+ });
+ app.post('/restaurante', function (req, res) {
+ 	var url = 'mongodb://localhost:27017/oxifood';
+
+ // Use connect method to connect to the server
+ MongoClient.connect(url, function(err, db) {
+   var collection = db.collection('comedoria');
+
+ 		var food = {
+ 			nameplace: req.body.nameplace,
+      phone: req.body.phone,
+ 			food1: req.body.food1,
+      food2: req.body.food2,
+      food3: req.body.food3,
+      food4: req.body.food4
+ 		};
+
+ 					collection.insertOne(food);
+ 					res.redirect('/');
+
+ 				db.close();
+
+ 	});
+ });
+
 
  app.listen(3000, function () {
  	console.log('Example app listening on port 3000!');
