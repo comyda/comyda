59392b820b9a8c21b4d0e4b31d73c23af56ae3c7
myllena <myllenaalvesdelima@outlook.com>
2017-01-03 17:05:17 -0300

    {Myllena e Zé} Criando controller para comedorias

--- services/calcular.js
+++ services/calcular.js
@@ -4,8 +4,8 @@ module.exports = {
     var numPizza = Math.ceil(subscribers/4);
     var totalPizza = numPizza * 27.00;
     return {numPizza, total: totalPizza};
-},
-/*CALCULO DO NÚMERO DE REFRIGERANTES E VALORES*/
+  },
+  /*CALCULO DO NÚMERO DE REFRIGERANTES E VALORES*/
   getGuarana: function(subscribers) {
     var numGuarana = Math.ceil(subscribers/8);
     var totalGuarana = numGuarana * 8.00;
