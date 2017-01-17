c12a2e9f44374b61148f2ca31cbd7ae982f9c3be
jcsfbass <jcsfbass@gmail.com>
2017-01-17 14:47:15 -0300

    Calculando preços das pizzas corretamente

--- services/resultado.js
+++ services/resultado.js
@@ -6,66 +6,70 @@ function getSodaTotal(participants) {
   return getSodaQuantity(participants) * 8;
 }
 
-function generateFoods(participants, comedoria) {
-  function extractChooses(participants) {
-    const chooses = [];
+function extractChooses(participants, comedoria) {
+  const chooses = [];
 
-    participants.forEach(participant => {
-      const foundFood = chooses.find(food => food.name === participant.flavor);
-      if (foundFood) {
-        foundFood.quantity++;
-      } else {
-        chooses.push({name: participant.flavor, quantity: 1});
-      }
-    });
-
-    return chooses;
-  }
-
-  function extractFoods(chooses, comedoria) {
-    const foods = [];
-    chooses.forEach((choose, index) => {
-      if (choose.quantity >= comedoria.dividedByPerson) {
-        const quantity = Math.floor(choose.quantity / comedoria.dividedByPerson);
-        foods.push({name: choose.name, quantity: quantity});
-        choose.quantity %= comedoria.dividedByPerson;
-        if (choose.quantity === 0) chooses.splice(index, 1);
-      }
-    });
-
-    return foods;
-  }
-
-  function insertRemainingFoods(chooses, foods, comedoria, remainingQuantityOfFoods) {
-    const remainingFoods = [];
-
-    chooses.forEach(choose => {
-      if (remainingQuantityOfFoods === chooses.length) {
-        foods.push({name: choose.name, quantity: 1});
-      } else if (remainingQuantityOfFoods >= chooses.length * 2) {
-        if (remainingFoods.length < remainingQuantityOfFoods * 2) {
-          remainingFoods.push({name: choose.name});
-        }
-      } else {
-        remainingFoods.push({name: choose.name});
-      }
-    });
-
-    for (let i = 0; i < remainingFoods.length; i += 2) {
-      if (remainingQuantityOfFoods === 0) break;
-
-      if (i === remainingFoods.length - 1) {
-        foods.push({name: remainingFoods[i].name, quantity: 1});
-        remainingQuantityOfFoods--;
-      } else {
-        foods.push({name: `${remainingFoods[i].name} e ${remainingFoods[i+1].name}`, quantity: 1});
-        remainingQuantityOfFoods--;
-      }
+  participants.forEach(participant => {
+    const foundFood = chooses.find(food => food.name === participant.flavor);
+    const foundFoodFromComedoria = comedoria.foods.find(food => food.name === participant.flavor);
+    if (foundFood) {
+      foundFood.quantity++;
+    } else {
+      chooses.push({name: participant.flavor, quantity: 1, price: foundFoodFromComedoria.price});
     }
+  });
 
+  return chooses;
+}
+
+function extractFoods(chooses, comedoria) {
+  const foods = [];
+  chooses.forEach((choose, index) => {
+    if (choose.quantity >= comedoria.dividedByPerson) {
+      const quantity = Math.floor(choose.quantity / comedoria.dividedByPerson);
+      foods.push({name: choose.name, quantity: quantity, price: choose.price});
+      choose.quantity %= comedoria.dividedByPerson;
+      if (choose.quantity === 0) chooses.splice(index, 1);
+    }
+  });
+
+  return foods;
+}
+
+function insertRemainingFoods(chooses, foods, comedoria, remainingQuantityOfFoods) {
+  const remainingFoods = [];
+
+  chooses.forEach(choose => {
+    if (remainingQuantityOfFoods === chooses.length) {
+      foods.push({name: choose.name, quantity: 1, price: choose.price});
+    } else if (remainingQuantityOfFoods >= chooses.length * 2) {
+      if (remainingFoods.length < remainingQuantityOfFoods * 2) {
+        remainingFoods.push({name: choose.name, price: choose.price});
+      }
+    } else {
+      remainingFoods.push({name: choose.name, price: choose.price});
+    }
+  });
+
+  for (let i = 0; i < remainingFoods.length; i += 2) {
+    if (remainingQuantityOfFoods === 0) break;
+
+    if (i === remainingFoods.length - 1) {
+      foods.push({name: remainingFoods[i].name, quantity: 1, price: choose.price});
+      remainingQuantityOfFoods--;
+    } else {
+      let price = undefined;
+      if (remainingFoods[i].price > remainingFoods[i+1].price) price = remainingFoods[i].price;
+      else price = remainingFoods[i+1].price;
+
+      foods.push({name: `${remainingFoods[i].name} e ${remainingFoods[i+1].name}`, quantity: 1, price});
+      remainingQuantityOfFoods--;
+    }
   }
+}
 
-  const chooses = extractChooses(participants);
+function generateFoods(participants, comedoria) {
+  const chooses = extractChooses(participants, comedoria);
   if (comedoria.dividedByPerson === 1) {
     return chooses;
   }
@@ -82,23 +86,20 @@ function generateFoods(participants, comedoria) {
   return foods;
 }
 
-function calculateTotal(participants, comedoria) {
-  let total = 0;
-  participants.forEach(participant => {
-    const foodFromComedoria = comedoria.foods.find(food => food.name === participant.flavor);
-    total += foodFromComedoria.price;
-  });
+function calculateTotal(foods, participants, comedoria) {
+  const totalFoods = foods.reduce((accumulator, food) => {
+    return accumulator + food.quantity * food.price;
+  }, 0);
 
-  total += getSodaTotal(participants);
-
-  return total;
+  return totalFoods + getSodaTotal(participants) + comedoria.deliveryRate;
 }
 
 module.exports = {
   calcular: (participants, comedoria) => {
+    const foods = generateFoods(participants, comedoria);
     return {
-      foods: generateFoods(participants, comedoria),
-      total: calculateTotal(participants, comedoria),
+      foods: foods,
+      total: calculateTotal(foods, participants, comedoria),
       sodaTotal: getSodaTotal(participants),
       sodaQuantity: getSodaQuantity(participants)
     };
