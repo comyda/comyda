c9f8e9e2d1b9a477bbcc6fb994080563efe3b40b
Leonardo <leooliveira3471@hotmail.com>
2017-01-19 17:16:29 -0300

    {Léo e Zé} WIP: terminando teste de criação evento

--- e2e/e2e_spec.js
+++ e2e/e2e_spec.js
@@ -1,5 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
 describe('Oxifood', () => {
   beforeEach(() => {
+    const url = 'mongodb://localhost:27017/oxifood';
+
+    MongoClient.connect(url, (err, db) => {
+      db.collection('eventos').deleteMany()
+      db.close();
+    });
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
   });
@@ -8,15 +17,14 @@ describe('Oxifood', () => {
     expect(browser.getTitle()).toEqual('oxiFood');
   });
 
-  xit('deve criar um evento', () => {
+  it('deve criar um evento', () => {
     element(by.className('button')).click();
     expect(browser.getTitle()).toEqual('Crie seu evento');
 
     element(by.tagName('select')).sendKeys('Brasileirinho');
     element(by.id('name')).sendKeys('Nome do responsavel');
     element(by.id('event')).sendKeys('Evento Teste');
-    element(by.id('time')).sendKeys('01202017 1200');
-
+    element(by.id('time')).sendKeys('200120171000');
     element(by.css('.botao input[type="submit"]')).click();
   });
 });
