3622aa01f9ce43e2c926e751db9da2a9f6a605dc
Leonardo <leooliveira3471@hotmail.com>
2017-01-19 14:48:16 -0300

    {Léo e Zé} WIP: fazendo teste que cria um evento

--- e2e/e2e_spec.js
+++ e2e/e2e_spec.js
@@ -1,8 +1,22 @@
 describe('Oxifood', () => {
-  it('deve ter um título', () => {
+  beforeEach(() => {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
+  });
 
+  it('deve ter um título', () => {
     expect(browser.getTitle()).toEqual('oxiFood');
+  });
+
+  xit('deve criar um evento', () => {
+    element(by.className('button')).click();
+    expect(browser.getTitle()).toEqual('Crie seu evento');
+
+    element(by.tagName('select')).sendKeys('Brasileirinho');
+    element(by.id('name')).sendKeys('Nome do responsavel');
+    element(by.id('event')).sendKeys('Evento Teste');
+    element(by.id('time')).sendKeys('01202017 1200');
+
+    element(by.css('.botao input[type="submit"]')).click();
   });
 });
